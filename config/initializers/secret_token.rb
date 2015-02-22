bf5c2a01d16ca6d73289bb134f89ca67a389c57e
Young <young.cha@hyfn.com>
2015-02-22 14:53:19 -0800

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeskChallenge::Application.config.secret_key_base = 'd00dec019173e3b2d8e41e8bc5175aa98a9ef788a7f6eb70d7b186ee9245f98388df85e784e1560c92d2eb83111c15c81c33bbefc376cdad29c7f6b86fe49198'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeskChallenge::Application.config.session_store :cookie_store, key: '_desk_challenge_session'
