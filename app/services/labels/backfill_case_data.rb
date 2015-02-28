cde0109ea41a72ad7c7e41ef9174c0204cf22169
Young <young.cha@hyfn.com>
2015-02-27 16:31:36 -0800

    fix backfill case data worker for labels

--- app/services/labels/backfill_case_data.rb
+++ app/services/labels/backfill_case_data.rb
@@ -1,12 +1,17 @@
 class Labels::BackfillCaseData
-  attr_reader :desk_case, :label_service_id
+  attr_reader :desk_case, :label, :label_service_id
   def initialize(case_id, label_service_id)
     @desk_case = Case.find(case_id)
     @label_service_id = label_service_id
+    @label = Label.find_by(service_id: label_service_id)
   end
 
   def perform
-    desk_case.labels.create(name: label_data.name, description: label_data.description, service_id: label_service_id)
+    if label.present?
+      desk_case.labels << label
+    else
+      desk_case.labels.create(name: label_data.name, description: label_data.description, service_id: label_service_id)
+    end
   end
 
   def label_data
